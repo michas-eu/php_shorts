1767a5764e1fcd874871a20cb69b86490a57f1d1
Michał Czarnecki <mczarnecki@m-t.pl>
2013-10-09 11:20:21 +0200

    Better decoding of links.

--- link_decode.php
+++ link_decode.php
@@ -4,24 +4,48 @@
 <?php
 $x = @$_POST['link'];
 if ($x):
-?>
-	<pre>
-<?php
 	$parts = parse_url($x);
 	$q = @$parts['query'];
 	if ($q):
 		unset($parts['query']);
-		var_dump($parts);
 		parse_str($q,$qa);
-		var_dump($qa);
+		$qp = '<br><br>'.htmlspecialchars(urldecode($q));
 	else:
-		var_dump($parts);
+		$qa = array();
+		$qp = false;
 	endif;
-?>
-	</pre>
-<?php
 endif;
 ?>
+
+<?php if ($x): ?>
+<?= $x; ?>
+<?= $qp; ?>
+<br><br>
+<table border>
+<?php foreach ($parts as $k => $v): ?>
+  <tr><th><?= $k ?></th><td><?= $v ?></td></tr>
+<?php endforeach; ?>
+  <tr><td colspan='2'> ### </td></tr>
+<?php foreach ($qa as $k => $v): ?>
+  <tr>
+    <th><?= $k ?></th>
+    <td>
+      <?php
+if (is_array($v)):
+	echo '<pre>';
+	print_r($v);
+	echo '</pre>';
+else:
+	echo $v;
+endif;
+      ?>
+    </td>
+  </tr>
+<?php endforeach; ?>
+</table>
+<br><br>
+<?php endif; ?>
+
 <form action='./link_decode.php' method='post'>
 <div>
   <textarea name='link'></textarea>
