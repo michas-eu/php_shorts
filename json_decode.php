d29f323cf63ef8a9e84bfdffc3c9016a132250fb
Michał Czarnecki <mczarnecki@m-t.pl>
2013-10-09 11:20:32 +0200

    Early json_decode

---
+++ json_decode.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<meta charset='utf-8'>
+<title>JSON decode</title>
+<?php
+$x = @$_POST['json'];
+if ($x):
+	$j = json_decode($x,true);
+endif;
+
+function print_nice_elm($a,$d=0) {
+	is_array($a);
+	foreach ($a as $k=>$v):
+		echo str_repeat('--',$d), ' ', $k, ': ';
+		if (is_array($v)):
+			echo 'Array';
+			print_nice_elm($v,$d+1);
+		else:
+			echo htmlspecialchars($v), "<br>\n";
+		endif;
+	endforeach;
+}
+?>
+
+<?php if ($j): ?>
+<pre>
+<?php print_nice_elm($j); ?>
+</pre>
+<br><br>
+<?php endif; ?>
+
+<form action='./json_decode.php' method='post'>
+<div>
+  <textarea name='json'></textarea>
+  <input type='submit'>
+</div>
+</form>
